d809b3ced388046542419407b991a58f6c008c81
luoyuxiang <async454619766@gmail.com>
2021-10-28 11:15:04 +0800

    do some

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          456
-        </a>
+        do some thing
       </header>
     </div>
   );
