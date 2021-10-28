e41fc6398f24d8ce08ba723104e1326f9d57f1d9
luoyuxiang <async454619766@gmail.com>
2021-10-28 14:15:58 +0800

    revert app.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+        {/*   Edit <code>src/App.js</code> and save to reload. */}
+        </p>
+        do some thing 1
+      </header>
+    </div>
+  );
+}
+
+export default App;
